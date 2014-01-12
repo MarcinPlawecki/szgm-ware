810858ce0a1589d4c2d3038f304d92ea0744a4d0
MarcinPlawecki <marcin.plawecki89@gmail.com>
2014-01-12 17:42:50 +0100

    asdas

--- src/main/java/szgm/towar/dao/TowarDaoImpl.java
+++ src/main/java/szgm/towar/dao/TowarDaoImpl.java
@@ -2,19 +2,19 @@ package szgm.towar.dao;
  
 import java.io.Serializable;
 import java.util.List;
- 
 
-import java.util.logging.Logger;
-
+import org.apache.log4j.Logger;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import szgm.towar.model.Towar;
+import szgm.towar.dao.TowarDao;
+
  
 public class TowarDaoImpl extends HibernateDaoSupport implements TowarDao, Serializable {
     	   
     private static final long serialVersionUID = 1L;
     
-    private final static Logger LOGGER = Logger.getLogger(TowarDaoImpl.class.getName());
+    private final static Logger LOGGER = Logger.getLogger(TowarDaoImpl.class);
  
 	public void addTowar(Towar towar){
 		getHibernateTemplate().save(towar);
